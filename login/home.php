c3aa90265d8b4681fd83675741c9f968f0e292f6
N Chaithanya <chaithanya.n2001@gmail.com>
2021-05-06 12:01:02 +0530

    php file

--- login/home.php
+++ login/home.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+     <h1>Thankyou! <?php echo $_SESSION['name']; ?></h1>
      <a href="logout.php">Logout</a>
 </body>
 </html>
