e61ac6a4f619ec847488d3f63bb89e338bfc1893
N Chaithanya <chaithanya.n2001@gmail.com>
2021-05-09 22:44:16 +0530

    Update numscroller-1.0.js

--- js/numscroller-1.0.js
+++ js/numscroller-1.0.js
@@ -1,11 +1,4 @@
-/**
-* jQuery scroroller Plugin 1.0
-*
-* http://www.tinywall.net/
-* 
-* Developers: Arun David, Boobalan
-* Copyright (c) 2014 
-*/
+
 (function($){
     $(window).on("load",function(){
         $(document).scrollzipInit();
